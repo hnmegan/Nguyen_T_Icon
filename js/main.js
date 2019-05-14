a38e5553041fd7226bb7b8992550468e0dea45eb
Nguyen <t_nguyen91265@tss-hr420-dm02.fc.ca>
2019-05-14 17:58:50 -0400

    First js script

--- js/main.js
+++ js/main.js
@@ -2,5 +2,23 @@
 
 // stub
  
-	consle.log('hello good morning') 
+	consle.log('fired'); 
+
+// select elements with Javascript
+// 
+// Setting up a variable using the let keyword,
+// And using a css selector to find the element we want to use
+// Let stores the reference to element as a variable ( in memory)
+
+	let svgGraphic = document.querySelector("#badgeSVG");
+
+	// functions are reusable pieces of code
+	// that you can run anytime
+
+	function logMyId() {
+		console.log(this.id);
+
+		this.style.opacity = 0.5;
+	}
+	svgGraphic.addEventListener("Click", logMyId);
 })();
