42b7890fc737e32d0aecc19e0073c7b7ff857a43
Megan <hnmegan@gmail.com>
2019-05-21 16:46:35 -0400

    js editting

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 (() => {
-
-// stub
- 
-	consle.log('fired'); 
+// stub 
+	console.log('fired'); 
 
 // select elements with Javascript
-// 
+
 // Setting up a variable using the let keyword,
 // And using a css selector to find the element we want to use
+
 // Let stores the reference to element as a variable ( in memory)
+	let svgGraphic = document.querySelector("#badgeSVG"),
+		mainHeadline = document.querySelector(".main-headline"),
+		subHeadline = document.querySelector("h3"),
+		swapTextButton = document.querySelector(".switch-type");
 
-	let svgGraphic = document.querySelector("#badgeSVG");
-
-	// functions are reusable pieces of code
+	// functions are reusable pieces of code - put these between the variable and the event handling
 	// that you can run anytime
 
 	function logMyId() {
@@ -20,5 +21,18 @@
 
 		this.style.opacity = 0.5;
 	}
-	svgGraphic.addEventListener("Click", logMyId);
+
+	function swapText(){
+		mainHeadline.textContent = "Baby shark doo doo doo";
+		mainHeadline.classList.toggle("selected");
+
+		subHeadline.textContent = "lol";
+		subHeadline.classList.toggle("selected");
+	}
+
+	//events always go down here
+
+	svgGraphic.addEventListener("click", logMyId);
+	swapTextButton.addEventListener("click", swapText);
+
 })();
